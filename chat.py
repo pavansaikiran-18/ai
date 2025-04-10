8d17dd79b1e81eed57faadd1df747bed9f48b2da
pavansaikiran-18 <nerellapavansaikiran@gmail.com>
2025-04-10 16:50:49 +0530

    first commit

---
+++ chat.py
@@ -0,0 +1,34 @@
+import base64
+import os
+import dotenv
+from google import genai
+from google.genai import types
+dotenv.config()
+
+def generate():
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.0-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_mime_type="text/plain",
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
